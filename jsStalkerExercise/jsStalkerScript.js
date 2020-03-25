991ecdd54fa59b8e7379e5e215ddcd262e65a0db
Sandy <55735816+sandyMax974@users.noreply.github.com>
2020-03-25 17:27:00 +0000

    Add files via upload

---
+++ jsStalkerExercise/jsStalkerScript.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt ("What is your first name?")
+var userLastName = prompt ("What is your last name?")
+var userAge = prompt ("What is your age?")
+
+console.log ("Your full name is " + userFirstName + " " +userLastName)
+console.log ("Your age is " + userAge)
