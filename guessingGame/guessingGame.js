9e73e70c742b78a6f5a8fbd4804174b57ae1e57f
Sandy <55735816+sandyMax974@users.noreply.github.com>
2020-04-15 17:16:35 +0100

    Add files via upload

--- guessingGame/guessingGame.js
+++ guessingGame/guessingGame.js
@@ -1,15 +1,12 @@
-let number = prompt("Enter your guess here");
-const value = 7;
+const secretNumber = 4;
+let guess = Number(prompt("Enter your guess here"));
 
-
-function guess(number){
-	if (number < value){
-		alert("not enough")
-	}
-	else if(number > value){
-		alert("too high")
-	}
-	else {
-		alert("corretc!")
-	}
+if (guess === secretNumber){
+	alert("You got it right!");
+}
+else if (guess > secretNumber){
+	alert("Too high. Guess again!")
+}
+else {
+	alert("Too low. Guess again!")
 }
